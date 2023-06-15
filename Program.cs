88fdb8b543a9f56c79af21e85f39ac78d0f39436
Chauncey Loving <chaunceys180@gmail.com>
2023-06-14 17:47:02 -0700

    Update .gitignore
    Creating an entry point into the application.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//This is the entry point into the application, this is needed for the test suites from NUnit to run. 
+static void Main(string[] args)
+{
+    
+}
